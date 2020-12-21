ee24362a5150f142a23dd80e23396de50ef4fb8a
Asierhernandezga <75242363+Asierhernandezga@users.noreply.github.com>
2020-12-21 09:40:27 +0100

    Test

---
+++ NavidadesProyecto/src/test/java/NavidadesProyecto/CalculosTest.java
@@ -0,0 +1,53 @@
+package NavidadesProyecto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculosTest {
+
+	private Calculos calculos = new Calculos();
+	private int resultadoInt;
+	private double resultadoDouble;
+	private int x, y;
+	@Test
+	public void testSuma() {
+		x=4;
+		y=7;
+		
+		resultadoInt = calculos.suma(x, y);
+		
+		org.junit.Assert.assertEquals(11, resultadoInt);
+	}
+	
+
+	@Test
+	public void testResta() {
+		x=4;
+		y=7;
+		
+		resultadoInt = calculos.resta(x, y);
+		
+		org.junit.Assert.assertEquals(-3, resultadoInt);
+}
+
+	@Test
+	public void testDivisionDistinto0Division() {
+		x=4;
+		y=7;
+		
+		resultadoDouble = calculos.division(x, y);
+		
+		org.junit.Assert.assertEquals(0.57, resultadoDouble, 0.002);
+	}
+	
+	@Test
+	public void testDivisionIgual0Division() {
+		x=4;
+		y=7;
+		
+		resultadoDouble = calculos.division(x, y);
+		
+		org.junit.Assert.assertEquals(-1, resultadoDouble, 0.002);
+	}	
+}
